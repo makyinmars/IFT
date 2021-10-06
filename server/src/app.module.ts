f801dc3b3cd693f9f2dad1be1ca95b63d0240b82
Maky <franklinjaradev@gmail.com>
2021-10-06 17:48:39 -0400

    chore: updating server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,11 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: "sqlite",
-      database: ":memory:",
-      entities: ["dist/**/*.entity{.ts,.js}"],
-      synchronize: true,
-    }),
-    UsersModule,
-  ],
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,8 @@
-import { NestFactory } from "@nestjs/core";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle("Users Crud")
-    .setDescription("The description")
-    .setVersion("1.0")
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("/", app, document);
-
   await app.listen(3000);
 }
 bootstrap();
