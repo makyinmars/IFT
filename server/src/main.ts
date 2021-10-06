a0540f1dfecd124fbf2cb7d6e4299435ad16f2a9
Maky <franklinjaradev@gmail.com>
2021-10-06 17:55:49 -0400

    chore: added swagger setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('IFT')
+    .setDescription('The IFT API description')
+    .setVersion('1.0.0')
+    .addTag('IFT')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
