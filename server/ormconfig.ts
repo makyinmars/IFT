53ed118bae9786a91808097b4d192be708987339
Maky <franklinjaradev@gmail.com>
2021-10-06 21:08:15 -0400

    chore: added ormconfig.ts

---
+++ server/ormconfig.ts
@@ -0,0 +1,12 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(<string>process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+};
+
+export default config;
