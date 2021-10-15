0916615ddfd99c93342b4d58690621e3dc6c5b7b
Maky <franklinjaradev@gmail.com>
2021-10-15 00:12:32 -0400

    chore: form for register

--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,4 +1,6 @@
 import { NextPage } from "next";
+import Link from "next/link";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import {
   VStack,
   Heading,
@@ -12,11 +14,33 @@ import {
   FormHelperText,
   Button,
 } from "@chakra-ui/react";
-import Link from "next/link";
-import React from "react";
+
+const userFormData = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+};
 
 const Register: NextPage = () => {
   const colSpan = useBreakpointValue({ base: 2, md: 1 });
+
+  const [formData, setFormData] = useState(userFormData);
+
+  const { firstName, lastName, email, password } = formData;
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData(userFormData);
+  };
 
   return (
     <VStack w="full" h="full" p={10}>
@@ -27,39 +51,61 @@ const Register: NextPage = () => {
         </Text>
       </VStack>
 
-      <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
-        <GridItem colSpan={colSpan}>
-          <FormControl id="firstName" isRequired>
-            <FormLabel htmlFor="firstName">First Name</FormLabel>
-            <Input type="text" placeholder="John" />
-          </FormControl>
-        </GridItem>
-        <GridItem colSpan={colSpan}>
-          <FormControl id="lastName" isRequired>
-            <FormLabel htmlFor="lastName">Last Name</FormLabel>
-            <Input type="text" placeholder="Doe" />
-          </FormControl>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <FormControl id="email" isRequired>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <Input type="email" placeholder="johndoe@gmail.com" />
-            <FormHelperText>We will never share your email.</FormHelperText>
-          </FormControl>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <FormControl id="password" isRequired>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input type="password" placeholder="123456" />
-            <FormHelperText>Please use a secure password.</FormHelperText>
-          </FormControl>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <Button w="full" variant="primary">
-            Sign Up
-          </Button>
-        </GridItem>
-      </SimpleGrid>
+      <form onSubmit={onSubmit}>
+        <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
+          <GridItem colSpan={colSpan}>
+            <FormControl id="firstName" isRequired>
+              <FormLabel htmlFor="firstName">First Name</FormLabel>
+              <Input
+                type="text"
+                placeholder="John"
+                value={firstName}
+                onChange={onChange}
+              />
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={colSpan}>
+            <FormControl id="lastName" isRequired>
+              <FormLabel htmlFor="lastName">Last Name</FormLabel>
+              <Input
+                type="text"
+                placeholder="Doe"
+                value={lastName}
+                onChange={onChange}
+              />
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <FormControl id="email" isRequired>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input
+                type="email"
+                placeholder="johndoe@gmail.com"
+                value={email}
+                onChange={onChange}
+              />
+              <FormHelperText>We will never share your email.</FormHelperText>
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <FormControl id="password" isRequired>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                type="password"
+                placeholder="123456"
+                value={password}
+                onChange={onChange}
+              />
+              <FormHelperText>Please use a secure password.</FormHelperText>
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <Button w="full" variant="primary" type="submit">
+              Sign Up
+            </Button>
+          </GridItem>
+        </SimpleGrid>
+      </form>
     </VStack>
   );
 };
