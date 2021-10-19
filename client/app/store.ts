38001499b64f7c01e4f78d4956bd3d061b52cc87
makyfj <65879341+makyfj@users.noreply.github.com>
2021-10-19 10:31:54 -0400

    chore: added redux-persistor config

--- client/app/store.ts
+++ client/app/store.ts
@@ -1,10 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import authSlice from "./features/auth/authSlice";
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    auth: authSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 // Infer the RootState and AppDispatch types from the store
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,24 +3,31 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import Head from "next/head";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
 import theme from "../src/theme/theme";
 import Container from "../src/components/container";
 import { store } from "../app/store";
 
+let persistor = persistStore(store);
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <ChakraProvider theme={theme}>
-        <Container>
-          <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1.0"
-            />
-          </Head>
-          <Component {...pageProps} />
-        </Container>
-      </ChakraProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider theme={theme}>
+          <Container>
+            <Head>
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+              />
+            </Head>
+            <Component {...pageProps} />
+          </Container>
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
   );
 }
