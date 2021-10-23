3709508fc9a7a2f1bd331907597404749d1f1067
Maky <franklinjaradev@gmail.com>
2021-10-22 22:48:01 -0400

    chore: adding image upload and preview for profile

--- client/src/components/user-profile.tsx
+++ client/src/components/user-profile.tsx
@@ -1,10 +1,11 @@
+import { ChangeEvent, useState, MouseEvent } from "react";
 import { useRouter } from "next/dist/client/router";
 import {
   Flex,
-  HStack,
   VStack,
   Heading,
   Text,
+  Image,
   Input,
   useColorModeValue,
   FormControl,
@@ -12,14 +13,21 @@ import {
   SimpleGrid,
   GridItem,
   useBreakpointValue,
+  Button,
 } from "@chakra-ui/react";
+
 import { useAppSelector } from "../../app/hooks";
-import React from "react";
 
 const UserProfile = () => {
   const router = useRouter();
-
   const { id } = router.query;
+  const [imageSelected, setImageSelected] = useState<File>(); // Also try <string | Blob>
+
+  const imageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setImageSelected(e.target.files[0]);
+    }
+  };
 
   const colSpan = useBreakpointValue({ base: 2, md: 1 });
   const bg = useColorModeValue("gray.200", "gray.700");
@@ -36,10 +44,56 @@ const UserProfile = () => {
         <Heading size="md">{`Franklin's Profile`}</Heading>
         <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
           <GridItem colSpan={colSpan}>
-            <FormControl>
-              <FormLabel>First Name</FormLabel>
+            <FormControl id="avatar">
+              <FormLabel htmlFor="avatar">Avatar</FormLabel>
+              <Input type="file" onChange={imageChange} />
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={colSpan} align="center">
+            {imageSelected && (
+              <Image
+                src={URL.createObjectURL(imageSelected)}
+                borderRadius="full"
+                boxSize="100px"
+                alt="image"
+              />
+            )}
+          </GridItem>
+          <GridItem colSpan={colSpan}>
+            <FormControl id="firstName">
+              <FormLabel htmlFor="firstName">First Name</FormLabel>
               <Input type="text" placeholder="John" />
             </FormControl>
+          </GridItem>
+          <GridItem colSpan={colSpan}>
+            <FormControl id="lastName">
+              <FormLabel htmlFor="lastName">Last Name</FormLabel>
+              <Input type="text" placeholder="Doe" />
+            </FormControl>
+          </GridItem>
+
+          <GridItem colSpan={2}>
+            <FormControl id="email">
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input type="text" placeholder="Doe" />
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <FormControl>
+              <FormLabel>Password</FormLabel>
+              <Input type="password" placeholder="*****" />
+            </FormControl>
+          </GridItem>
+
+          <GridItem colSpan={colSpan}>
+            <Button w="full" variant="primary" type="submit">
+              Sign Up
+            </Button>
+          </GridItem>
+          <GridItem colSpan={colSpan}>
+            <Button w="full" variant="primary" type="submit">
+              Sign Up
+            </Button>
           </GridItem>
         </SimpleGrid>
       </VStack>
